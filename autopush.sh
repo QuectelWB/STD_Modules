ed010a07e32cfc1940cac8aaad64fc17f5ce7ad1
QuectelWB <panpanwang@aliyun.com>
2020-10-19 18:30:48 +0800

    Mon Oct 19 18:30:46 CST 2020

--- autopush.sh
+++ autopush.sh
@@ -5,19 +5,17 @@ git add .
 
 git status
 
-#写个sleep 1s 是为了解决并发导致卡壳
-
 sleep 2s
 
-echo "####### 添加文件 #######"
+echo "####### git add done #######"
 
 git commit -m "$commit_msg"
 
-echo "####### commit #######"
+echo "####### commit start######"
 
 sleep 2s
 
-echo "####### 开始推送 #######"
+echo "####### commit done, start git push #######"
 
 
 git push -u origin main
@@ -26,4 +24,4 @@ sleep 1s
 
 #QuectelWB
 
-echo "####### 推送成功 #######"
+echo "####### git push done #######"
