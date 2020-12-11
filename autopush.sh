0018e7b5606fbb16e36c4b6cd08bd6fa8fb0187f
QuectelWB <panpanwang@aliyun.com>
2020-12-11 18:08:20 +0800

    Fri Dec 11 18:08:17 CST 2020

--- autopush.sh
+++ autopush.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 commit_msg=`date`
 
 
@@ -17,11 +19,29 @@ sleep 2s
 
 echo "####### commit done, start git push #######"
 
+/usr/bin/expect << EOF
 
-git push -u origin ch-zh
 
-sleep 1s
+spawn git push -u origin ch-zh
+
+set timeout 3
+
+expect "Username for 'https://github.com':"
+
+send "QuectelWB\r\n"
+
+set timeout 10
+
+expect "Password for 'https://QuectelWB@github.com':"
+
+send "wangbees09\r\n"
+
+expect eof
+
+EOF
 
 #QuectelWB
 
 echo "####### git push done #######"
+
+exit
